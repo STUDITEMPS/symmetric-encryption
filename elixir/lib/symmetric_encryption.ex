41bf6bbafff2d1efd4485d855c1b949b9bf054c8
Silvan Buedenbender <silvan.buedenbender@studitemps.de>
2022-07-06 15:01:23 +0000

    Initialize elixir package

---
+++ elixir/lib/symmetric_encryption.ex
@@ -0,0 +1,18 @@
+defmodule SymmetricEncryption do
+  @moduledoc """
+  Documentation for `SymmetricEncryption`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SymmetricEncryption.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
