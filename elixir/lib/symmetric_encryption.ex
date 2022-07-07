851274076afcfb6aa00048e495ed5a7d755e557d
Kai Kuchenbecker <kuchenbecker.k@gmail.com>
2022-07-07 16:59:24 +0200

    Add documentation, improve spec

--- elixir/lib/symmetric_encryption.ex
+++ elixir/lib/symmetric_encryption.ex
@@ -1,6 +1,24 @@
 defmodule SymmetricEncryption do
   @moduledoc """
-  Documentation for `SymmetricEncryption`.
+  Provides symmetric encryption and decryption using AES-256 in Galois Counter Mode.
+
+  Provides a convenient encrypt/2 method that ensures that best practices are
+  followed in using AES-256-GCM.
+
+  We can use a single key for up to ~6 billion (6,074,001,001) encryptions until
+  the likelihood of reusing an IV does not satisfy the uniqueness requirement of
+  the NIST anymore.
+  IV key length is strictly 96 bits, since otherwise we'd have to rotate the key
+  after ~4.3 billion (2^32) invocations and the underlying GCM implementation
+  has to spend extra CPU cycles for converting the IV to a 96 bit IV.
+
+  The length of the returned authentication tag is 16 bytes and decryption only
+  accepts tags of that length, since otherwise attackers could provide tags of 1
+  byte length and their chance of correctly guessing the tag increases to 1/256.
+
+  See
+  https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-38d.pdf
+  for a detailed discussion and recommendations.
   """
 
   @tag_length 16
@@ -8,6 +26,21 @@ defmodule SymmetricEncryption do
   @encrypt true
   @decrypt not @encrypt
 
+  @doc """
+  Encrypts binary data using a 256 bit key and a generated 96 bit initialization
+  vector using AES-256-GCM.
+
+  Returns `{:ok, {initialization_vector, encrypted_data, tag}}`.
+  The initialization vector is required to decrypt the encrypted data. The tag
+  is used to validate the authenticity of the encrypted data (authenticated
+  encryption).
+
+  ## Examples
+
+    iex> key = :crypto.strong_rand_bytes(32)
+    iex> "secret text" |> SymmetricEncryption.encrypt(key)
+    {:ok, {<<_::96>>, <<_::88>>, <<_::128>>}}
+  """
   @spec encrypt(data :: binary(), key :: <<_::256>>) ::
           {:ok, {iv :: <<_::96>>, encrypted_data :: binary(), tag :: <<_::128>>}}
   def encrypt(data, key) do
@@ -16,8 +49,39 @@ defmodule SymmetricEncryption do
     encrypt(data, key, initialization_vector)
   end
 
+  @doc """
+  Encrypts binary data using a 256 bit key and a given initialization vector (IV) using AES-256-GCM. It is recommended that the IV is 96 bits long.
+
+  **IMPORTANT: An IV must never be reused with the same key!**
+  Reusing an IV-Key-Pair is catastrophic for AES-GCM, see
+  https://csrc.nist.gov/csrc/media/projects/block-cipher-techniques/documents/bcm/comments/800-38-series-drafts/gcm/joux_comments.pdf
+
+  Optionally accepts additional authenticated data (AAD) which is signed with
+  the authentication key and can be verified during decryption. This AAD is
+  *NOT* encrypted.
+
+  Returns `{:ok, {initialization_vector, encrypted_data, tag}}`.
+  The initialization vector is required to decrypt the encrypted data. The tag
+  is used to validate the authenticity of the encrypted data (authenticated
+  encryption).
+
+  **NOTE**: Authenticated additional data (AAD) is required to be the same
+  during encryption and decryption for decryption to succeed, i.e., when
+  passing AAD to encrypt/4 make sure to also pass it to decrypt/5.
+
+  ## Examples
+
+    iex> key = :crypto.strong_rand_bytes(32)
+    iex> iv = :crypto.strong_rand_bytes(12)
+    iex> "secret text" |> SymmetricEncryption.encrypt(key, iv)
+    {:ok, {<<_::96>>, <<_::88>>, <<_::128>>}}
+
+    iex> aad = "encrypted by Jim"
+    iex> "secret text" |> SymmetricEncryption.encrypt(key, iv, aad)
+    {:ok, {<<_::96>>, <<_::88>>, <<_::128>>}}}
+  """
   @spec encrypt(data :: binary(), key :: <<_::256>>, iv :: binary(), aad :: binary()) ::
-          {:ok, {binary(), binary(), binary()}}
+          {:ok, {iv :: <<_::96>>, encrypted_data :: binary(), tag :: <<_::128>>}}
   def encrypt(data, key, initialization_vector, authenticated_additional_data \\ "") do
     {encrypted_data, tag} =
       :crypto.crypto_one_time_aead(
@@ -39,6 +103,20 @@ defmodule SymmetricEncryption do
       end
   end
 
+  @doc """
+  Decrypts encrypted data with given key and initialization vector (IV) and
+  validates authenticity of encrypted data and optionally additional data (AAD)
+  using the provided tag.
+
+  Accepts only tags of 128 bit length (16 bytes).
+
+  **NOTE**: When authenticated additional data was passed during encryption it
+  must also be included during decryption, otherwise decryption will fail.
+
+  ## Examples
+    iex> SymmetricEncryption.decrypt(encrypted_text, key, iv, tag)
+    {:ok, "secret text"}
+  """
   @spec decrypt(
           encrypted_data :: binary(),
           key :: <<_::256>>,
@@ -70,5 +148,5 @@ defmodule SymmetricEncryption do
   end
 
   defp validate_tag_length(tag) when byte_size(tag) == @tag_length, do: {:ok, tag}
-  defp validate_tag_length(tag), do: {:error, :invalid_tag_length}
+  defp validate_tag_length(_tag), do: {:error, :invalid_tag_length}
 end
